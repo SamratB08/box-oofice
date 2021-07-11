f5f286cd08cbc06385812d427bfe0ed27c99cf2b
SmratB08 <samratbasak3@gmail.com>
2021-07-11 10:59:07 +0530

    Updates

---
+++ precache-manifest.d375471dddf85d51e270e131d0e238f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc46bab6cb7eae81b13b9616d547da23",
+    "url": "/box-oofice/index.html"
+  },
+  {
+    "revision": "b1355a2439006ee9fbb3",
+    "url": "/box-oofice/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "acf3488ff840ab064a6e",
+    "url": "/box-oofice/static/js/2.7e53af90.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-oofice/static/js/2.7e53af90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1355a2439006ee9fbb3",
+    "url": "/box-oofice/static/js/main.3bc3a02a.chunk.js"
+  },
+  {
+    "revision": "a48be9c608b4aaa3c782",
+    "url": "/box-oofice/static/js/runtime-main.8efe10df.js"
+  }
+]);
